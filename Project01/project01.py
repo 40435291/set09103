b1bfe0dd75aba812a7505284d053a008b4ee19d1
Eugene Gerber <40435291@live.napier.ac.uk>
2020-11-09 23:42:08 -0800

    flaskNote - Adding Bootstrap

--- Project01/project01.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-	return '<div><div><h1>Project01 - Advanced Web Technologies</h1><p><h2>Hello Napier!!!</h2></p></div></div>'
