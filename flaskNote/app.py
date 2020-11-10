3c2b8b1cf8f7d0d6e85e0da133af86bd4172862b
Eugene Gerber <40435291@live.napier.ac.uk>
2020-11-10 12:49:13 -0800

    Added Test-Data file

--- flaskNote/app.py
+++ flaskNote/app.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template
+from data import Notes 
 
 app = Flask(__name__)
+
+Notes = Notes()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/notes')
+def notes():
+    return render_template('notes.html', notes = Notes)
+
+@app.route('/note/<string:id>/')
+def note(id):
+    return render_template('note.html', id=id)    
+
+@app.route('/help')
+def help():
+    return render_template('help.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-# Debug Mode: On = (debug=True), else remove
+# Debug Mode: On = (debug=True)
---
+++ flaskNote/data.py
@@ -0,0 +1,25 @@
+def Notes():
+    notes = [
+        {
+            'id': 1,
+            'title': 'Note One',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, temporibus ipsa possimus consequuntur, totam voluptas sapiente perferendis, doloremque veniam unde amet vitae minus ratione molestias sunt error nemo dolorem recusandae.',
+            'author': 'Piet Pompies',
+            'create_date': '2020-11-09'
+            },
+        {
+            'id': 2,
+            'title': 'Note Two',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, temporibus ipsa possimus consequuntur, totam voluptas sapiente perferendis, doloremque veniam unde amet vitae minus ratione molestias sunt error nemo dolorem recusandae.',
+            'author': 'Jan Pampoen',
+            'create_date': '2020-10-20'
+            },
+        {
+            'id': 3,
+            'title': 'Note Three',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, temporibus ipsa possimus consequuntur, totam voluptas sapiente perferendis, doloremque veniam unde amet vitae minus ratione molestias sunt error nemo dolorem recusandae.',
+            'author': 'Koos Kakebeen',
+            'create_date': '2020-01-01'
+        }
+    ]
+    return notes
