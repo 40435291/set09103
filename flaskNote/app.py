c32677551a76271f0ff66d735485be5a6387bb78
Eugene Gerber <40435291@live.napier.ac.uk>
2020-11-11 14:19:27 -0800

    Added Registration form

--- flaskNote/app.py
+++ flaskNote/app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
-from data import Notes 
+from flask import Flask, render_template, flash, redirect, request, url_for, session, logging
+from data import Notes
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt  
 
 app = Flask(__name__)
 
@@ -15,12 +18,28 @@ def notes():
 
 @app.route('/note/<string:id>/')
 def note(id):
-    return render_template('note.html', id=id)    
+    return render_template('note.html', id=id)
 
 @app.route('/help')
 def help():
     return render_template('help.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        return render_template('register.html')
+    return render_template('register.html', form=form)
+
+class RegisterForm(Form):
+    name = StringField('Name:', [validators.Length(min=1, max=50)])    
+    username = StringField('Username:', [validators.Length(min=4, max=25)]) 
+    email = StringField('Email:', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password:', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='The Passwords entered do not match')
+    ])
+    confirm = PasswordField('Repeat Password:')
 
 if __name__ == '__main__':
     app.run(debug=True)
